e8cada63f4fbe398d4b299a3800d26ca8f71cb16
zhangwei <396033084@qq.com>
2019-06-28 16:24:00 +0800

    change app to AppProStater

---
+++ src/main/java/com/nuoyun/pro/AppProStarter.java
@@ -0,0 +1,24 @@
+package com.nuoyun.pro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * @Title:  AppProStarter.java
+ * @Package com.nuoyun.pro
+ * @Description: 工程启动类
+ * @author: zhangwei
+ * @date:   2019-06-28
+ * @version V1.0
+ * @Copyright: 2019 nuoyun All rights reserved.
+ */
+@SpringBootApplication
+@Slf4j
+public class AppProStarter {
+	public static void main(String[] args) {
+		SpringApplication.run(AppProStarter.class, args);
+		log.info("========== Nuoyun Pro 服务启动完成 ==========");
+	}
+}
