fa83fbd3494732e406289ef8c20f1c60dadab259
zhangwei <396033084@qq.com>
2019-06-28 08:51:44 +0800

    init

---
+++ src/main/java/com/nuoyun/pro/App.java
@@ -0,0 +1,13 @@
+package com.nuoyun.pro;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
